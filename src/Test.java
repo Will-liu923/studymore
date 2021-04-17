a44d0bdc4daf945b72d261aff978a00fa487ad84
Will Liu <leoLW923@outlook.com>
2021-04-18 00:16:47 +0800

    something wrong with Test,it would nice to rewrite it

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,6 @@
 public class Test {
     public static void main(String[] args){
         System.out.println("Hi,Java");
+        System.out.println("Hello world,it's a nice day to learn java");
     }
 }
